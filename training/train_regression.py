4ece512f6667478c96fec73858fc01d0ccd5b478
GosseMan <psj14207@snu.ac.kr>
2020-06-30 10:33:50 +0900

    0630regression

--- training/train_regression.py
+++ training/train_regression.py
@@ -213,7 +213,7 @@ def main():
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])  for x in ['train', 'val']}
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size,shuffle=True, num_workers=4) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
-
+    print(image_datasets['val'])
     if args.network == 'resnet18':
         model_ft = models.resnet18(pretrained=True)
         if not args.fine_tuning:
