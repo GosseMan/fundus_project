ace0a5511763bb1b18459271229938b2c5d074aa
GosseMan <psj14207@snu.ac.kr>
2020-06-30 15:56:32 +0900

    0630regression

--- training/gradcam_regression.py
+++ training/gradcam_regression.py
@@ -31,7 +31,7 @@ def get_class_name(c):
 
 def GradCAM(img, c, features_fn, classifier_fn):
     feats = features_fn(img.cuda())
-    print(feats.shape())
+    print(feats.size())
     #feats = features_fn(img.cuda())
     _, N, H, W = feats.size()
     out = classifier_fn(feats)
