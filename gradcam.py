932cbe5733fc9c4ebc155d27ea1e2c1ebb7d64f8
GosseMan <psj14207@snu.ac.kr>
2020-07-01 11:08:15 +0900

    0701

--- gradcam.py
+++ gradcam.py
@@ -248,8 +248,8 @@ def main():
     #img_list = ['vk038873-clahe.jpg','vk042499-clahe.jpg','vk080873-clahe.jpg','vk123312-clahe.jpg','vk127891-clahe.jpg']
     #img_path = "data/age_resized_clahe_split/val/vk034698.jpg"
     img_list = ['vk029159-clahe.jpg','vk029719-clahe.jpg', 'vk029742-clahe.jpg']
-    if not os.path.isdir('./Oh_gc'):
-        os.makedirs('./Oh_gc')
+    if not os.path.isdir('./gc'):
+        os.makedirs('./gc')
     for img in img_list:
         img_path = './'+img
         print(img_path)
