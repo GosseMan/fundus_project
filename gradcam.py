e8af292d672fd9c3a65ce7aa988a604bb2e95a3f
GosseMan <psj14207@snu.ac.kr>
2020-07-01 11:53:07 +0900

    0701

--- gradcam.py
+++ gradcam.py
@@ -235,7 +235,7 @@ def execute_all(model, target_layer, img_path, gcam_path, paper_cmap=True):
 def main():
     '''
     model_type = "Densenet169"
-    model_path = "../0630age_result/3_densenet169_model.pt"
+    model_path = "../0630age_result/3_densenet169_model_s.pt"
 
     #model = models.load(model_type)
     model.load_state_dict(torch.load(model_path))
@@ -249,15 +249,15 @@ def main():
     #img_list = ['vk038873-clahe.jpg','vk042499-clahe.jpg','vk080873-clahe.jpg','vk123312-clahe.jpg','vk127891-clahe.jpg']
     #img_path = "data/age_resized_clahe_split/val/vk034698.jpg"
     img_list = ['vk029159-clahe.jpg','vk029719-clahe.jpg', 'vk029742-clahe.jpg']
-    if not os.path.isdir('./Oh_gc_age'):
-        os.makedirs('./Oh_gc_age')
+    if not os.path.isdir('./Oh_gc_sex'):
+        os.makedirs('./Oh_gc_sex')
     for img in img_list:
         img_path = './'+img
         print(img_path)
 
         for target_layer in target_layer_lst:
 
-            result_path = "./Oh_gc_age/"+img.split('.')[0]+'_'+target_layer+'.jpg'
+            result_path = "./Oh_gc_sex/"+img.split('.')[0]+'_'+target_layer+'.jpg'
 
 
 
