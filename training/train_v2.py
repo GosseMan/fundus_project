b0bbb9ce8b2d07ec8a07c879020018360b0e9d3c
GosseMan <psj14207@snu.ac.kr>
2020-07-17 18:26:01 +0900

    0717

--- training/train_v2.py
+++ training/train_v2.py
@@ -67,6 +67,7 @@ def train_model(model,image_datasets, dataloaders,batch_size, criterion, optimiz
     best_loss = 10000
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     earlystop = 0
+    earlystop_final = False
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
